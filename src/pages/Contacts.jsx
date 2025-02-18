f475e2b0a7a945b2099a4b73c97fc9cce94e06f7
jaanus <zaar2213@gmail.com>
2025-02-18 14:35:26 +0200

    added data copy to contact click and updated devops to use CloudFront

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import LinkedIn from '../assets/images/LinkedIn1.jpg';
 import GitHub from '../assets/images/GitHub1.jpg';
@@ -10,9 +10,44 @@ const Contacts = () => {
   const addressRef = useRef();
   const email = useRef();
 
+  const [showName, setShowName] = useState('Jaanus Saar');
+  const [showPhone, setShowPhone] = useState('+37258218417');
+  const [showAddress, setShowAddress] = useState('Europe, Estonia, Tallinn');
+  const [showEmail, setShowEmail] = useState('Zaar2213@gmail.com');
+
   useEffect(() => {
     document.title = 'Contacts';
   }, []);
+
+  const copyName = () => {
+    navigator.clipboard.writeText(fullNameRef.current.textContent);
+    setShowName('Copied to Clipboard');
+    setTimeout(() => {
+      setShowName('Jaanus Saar');
+    }, 1000);
+  };
+
+  const copyPhone = () => {
+    navigator.clipboard.writeText(phoneRef.current.textContent);
+    setShowPhone('Copied to Clipboard');
+    setTimeout(() => {
+      setShowPhone('+37258218417');
+    }, 1000);
+  };
+  const copyAddress = () => {
+    navigator.clipboard.writeText(addressRef.current.textContent);
+    setShowAddress('Copied to Clipboard');
+    setTimeout(() => {
+      setShowAddress('Europe, Estonia, Tallinn');
+    }, 1000);
+  };
+  const copyEmail = () => {
+    navigator.clipboard.writeText(email.current.textContent);
+    setShowEmail('Copied to Clipboard');
+    setTimeout(() => {
+      setShowEmail('Zaar2213@gmail.com');
+    }, 1000);
+  };
 
   return (
     <div className={styles.picture}>
@@ -21,40 +56,24 @@ const Contacts = () => {
           <p
             className={styles.contactInfo}
             ref={fullNameRef}
-            onClick={() => {
-              navigator.clipboard.writeText(fullNameRef.current.textContent);
-            }}
+            onClick={copyName}
           >
-            Jaanus Saar
+            {showName}
           </p>
-          <div
-            className={styles.contactInfo}
-            onClick={() => {
-              navigator.clipboard.writeText(phoneRef.current.textContent);
-            }}
-          >
-            <p>Phone:</p>
-            <p ref={phoneRef}>+37258218417</p>
+          <div className={styles.contactInfo} onClick={copyPhone}>
+            <p ref={phoneRef}>{showPhone}</p>
           </div>
         </div>
         <div className={styles.contactInfoSlide}>
           <p
             className={styles.contactInfo}
             ref={addressRef}
-            onClick={() => {
-              navigator.clipboard.writeText(addressRef.current.textContent);
-            }}
+            onClick={copyAddress}
           >
-            Europe, Estonia, Tallinn
+            {showAddress}
           </p>
-          <p
-            className={styles.contactInfo}
-            ref={email}
-            onClick={() => {
-              navigator.clipboard.writeText(email.current.textContent);
-            }}
-          >
-            Zaar2213@gmail.com
+          <p className={styles.contactInfo} ref={email} onClick={copyEmail}>
+            {showEmail}
           </p>
         </div>
       </div>
