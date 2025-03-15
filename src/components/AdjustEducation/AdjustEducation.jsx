4c8f2c7bcf7326e8f2cbb79c8edaeae1c325f081
jaanus <zaar2213@gmail.com>
2025-03-15 10:25:23 +0200

    added education deletion and adding to CV

--- src/components/AdjustEducation/AdjustEducation.jsx
+++ src/components/AdjustEducation/AdjustEducation.jsx
@@ -2,13 +2,15 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 const API_SECRET = import.meta.env.VITE_API_SECRET || 'localhost';
 
 import { useEffect, useState, useRef } from 'react';
-
 import EducationModule from '../EducationModule/EducationModule';
+
+import styles from './AdjustEducation.module.css';
 
 const AdjustEducation = () => {
   const [educationList, setEducationList] = useState([]);
   const [openTab, setOpenTab] = useState(null);
   const title = useRef();
+  const img = useRef();
   const name = useRef();
   const date = useRef();
   const duration = useRef();
@@ -17,23 +19,23 @@ const AdjustEducation = () => {
   const cert = useRef();
 
   const submitHandler = async () => {
+    const formData = new FormData();
+    formData.append('title', title.current?.value);
+    formData.append('name', name.current?.value);
+    formData.append('date', date.current?.value);
+    formData.append('duration', duration.current?.value);
+    formData.append('desc', desc.current?.value);
+    formData.append('website', website.current?.value);
+    formData.append('cert', cert.current?.value);
+    formData.append('img', img.current?.files[0]);
     try {
       const response = await fetch(`${API_URL}/postEducation`, {
         method: 'POST',
         headers: {
           'X-Client-Secret': `${API_SECRET}`,
-          'Content-Type': 'application/json',
         },
         credentials: 'include',
-        body: JSON.stringify({
-          title: title.current.value,
-          name: name.current.value,
-          date: date.current.value,
-          duration: duration.current.value,
-          desc: desc.current.value,
-          website: website.current.value,
-          cert: cert.current.value,
-        }),
+        body: formData,
       });
 
       if (!response.ok) {
@@ -107,20 +109,51 @@ const AdjustEducation = () => {
 
   return (
     <div>
-      {educationList
-        .slice(0)
-        .reverse()
-        .map((listItem) => (
-          <EducationModule
-            key={listItem.id}
-            listData={listItem}
-            openTab={openTab}
-            setOpenTab={setOpenTab}
-            removeEducationHandler={removeEducationHandler}
-          />
-        ))}
+      <div className={styles.educationContainer}>
+        {educationList
+          .slice(0)
+          .reverse()
+          .map((listItem) => (
+            <EducationModule
+              key={listItem.id}
+              listData={listItem}
+              openTab={openTab}
+              setOpenTab={setOpenTab}
+              removeEducationHandler={removeEducationHandler}
+            />
+          ))}
+      </div>
+      <div className={styles.adjustEducation_add}>
+        <div className={styles.adjustEducation_add_titles}>
+          <span>Title</span>
+          <span>Image</span>
+          <span>Name</span>
+          <span>Date</span>
+          <span>Duration</span>
+          <span>Description</span>
+          <span>Website</span>
+          <span>Certificate</span>
+        </div>
+        <div className={styles.adjustEducation_add_inputs}>
+          <input type='text' ref={title} placeholder='Title'></input>
+          <input type='file' ref={img} placeholder='Image'></input>
+          <input type='text' ref={name} placeholder='Name'></input>
+          <input type='text' ref={date} placeholder='Date'></input>
+          <input type='text' ref={duration} placeholder='Duration'></input>
+          <input type='text' ref={desc} placeholder='Description'></input>
+          <input type='text' ref={website} placeholder='Website'></input>
+          <input type='text' ref={cert} placeholder='Certificate'></input>
+        </div>
+
+        <button
+          className={styles.adjustEducation_add_button}
+          type='submit'
+          onClick={submitHandler}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 };
-
 export default AdjustEducation;
